ce308b143b85abf20a2c3ebc503c35cf1758750f
felix <felix@volny.co>
2016-09-12 22:26:37 +0700

    refactored startTimer

--- app/index.js
+++ app/index.js
@@ -2,10 +2,6 @@ import './style.scss';
 import $ from 'jquery';
 import './flipclock.min.js';
 
-// FLIPCLOCK INSTANCE
-// issue: putting duration here weirdly brakes `clock.reset()` - get use `setTime`
-// issue: putting duration here makes clock lag behind by one second
-//var clock = $('#clock').FlipClock(duration, {
 const clock = $('#clock').FlipClock({
   autoStart: false,
   //clockFace: 'HourlyCounter',
@@ -15,20 +11,12 @@ const clock = $('#clock').FlipClock({
     start,
     stop,
     interval() {
-    // HOW TO USE FLIPCLOCK METHODS
-      var time = this.factory.getTime().time;
-      if(time) {
-        console.log('interval', time);
-      }
+      console.log('interval', this.factory.getTime().time);
     }
   }
 });
 
-function makeTimeString (time) {
-  let minutes = parseInt(time / 60, 10);
-  let seconds = parseInt(time % 60, 10);
-  return minutes + " Minutes " + seconds + " Seconds";
-}
+let mode = 'sesssion';
 
 function reset() {
   $('#sessionContainer').hide();
@@ -38,41 +26,40 @@ function reset() {
 function start() {
 }
 
-let mode = 'sesssion';
-
 function stop() {
-  console.log(mode);
   // TODO hack bc `stop` event is fired with 1 sec left on clock??
   window.setTimeout(() => startTimer(mode), 1000);
 }
 
 function startTimer() {
-  const duration = mode === 'session' ? document.querySelector('#sessionInput').value : document.querySelector('#breakInput').value;
-  // if we got values change from menu to timer screen
-  // TODO should everything in this func be only if (duration)?
-  if (duration) {
+  const sessionLength = parseInt(document.querySelector('#sessionInput').value);
+  const breakLength = parseInt(document.querySelector('#breakInput').value);
+
+  if (sessionLength && breakLength) {
+
     $('#sessionContainer').show();
     $('#menuContainer').hide();
+
+    if (mode === 'break') {
+      $('#session').text('Break Time :)');
+      $('.break-button').show();
+
+      clock.setTime(breakLength);
+      clock.start();
+
+      mode = 'session';
+    } else {
+      $('#session').empty();
+      $('.break-button').hide();
+
+      clock.setTime(sessionLength);
+      clock.start();
+
+      mode = 'break';
+    }
   } else {
     console.debug('no value given');
   }
-
-  // put the session name subheadline in the DOM
-  const session = $('#session');
-  if (mode === 'break') {
-    session.text('Break Time :)');
-    $('.break-button').show();
-  } else {
-    session.text('');
-    $('.break-button').hide();
-  }
-
-  // start the timer
-  clock.setTime(duration);
-  clock.start();
-
-  // change mode
-  mode = mode === 'session' ? 'break' : 'session';
 }
 
 $('#restart').click(() => {mode = 'session'; startTimer()});
